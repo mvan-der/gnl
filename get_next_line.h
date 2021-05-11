82c05770b0d5dd13ae49a44a5bb4946c9b376aea
Maarten van der Vlugt <mvan-der@student.codam.nl>
2021-05-11 11:03:11 +0200

    refactored

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mvan-der <mvan-der@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/24 11:22:26 by mvan-der      #+#    #+#                 */
-/*   Updated: 2021/05/05 18:51:23 by mvan-der      ########   odam.nl         */
+/*   Updated: 2021/05/11 11:02:36 by mvan-der      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,10 @@
 # include <unistd.h>
 
 int		get_next_line(int fd, char **line);
-int		left_overs(char **line, char *result);
 int		get_line(char **line, char *result, int j);
 int		find_newline(char *buffer);
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
+void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*gnl_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
